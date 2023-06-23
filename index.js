749beae82bda9207e56695953f8e18eba3f4eb2c
redrover9 <grace.thompson97@gmail.com>
2023-06-23 16:23:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const uri = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(uri);
 
 async function selectVerse() {
-  try {
+    try {
+    await client.connect();
     const database = client.db('sefaria');
     const texts = database.collection('texts');
 
@@ -17,42 +18,32 @@ async function selectVerse() {
       projection: { _id: 0, title: 1, chapter: 1 },
     };
 
-    var cursor = texts.find(bookQuery, bookOptions);
-
+    const uniqueBooks = texts.find(bookQuery, bookOptions);
     var books = [];
-    for await (const book of cursor) {
+    for await (const book of uniqueBooks) {
       books.push(book.title)
     }
-
     var solution = [];
     const selectedBook = uniqueRandomArray(books);
     solution.push(selectedBook());
 
     const chapterQuery = { title: solution[0], versionTitle: "Tanakh: The Holy Scriptures, published by JPS", purchaseInformationImage: "https://storage.googleapis.com/sefaria-physical-editions/JPS_1985.png" };
-    const chapterOptions = {
-      sort: { chapter: 1 },
-      projection: { _id: 0, chapter: 1},
-    };
 
-    var cursor = texts.find(chapterQuery, chapterOptions);
-
+    const uniqueChapters = await texts.distinct("chapter", chapterQuery);
     var chapters = [];
-    for await (const chapter of cursor) {
-      chapters.push(chapter.chapter);
+    for await (const chapter of uniqueChapters) {
+      chapters.push(chapter);
     }
 
-    const selectedChapter = uniqueRandomArray(chapters[0]);
+    const selectedChapter = uniqueRandomArray(chapters);
     const selectedVerse = uniqueRandomArray(selectedChapter());
     solution.push(selectedVerse());
     solution[1] = solution[1].replace(/<[^>]*>/g, "");
     return solution;
-
   } finally {
     await client.close();
-  }
-
+  }  
 }
-
 (async () => {
   console.log(await selectVerse())
 })()
